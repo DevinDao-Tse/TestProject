7d034f2c341a6415c4149f87f26dc02f4f478ca6
Devin Dao-Tse <devindaotse@gmail.com>
2023-01-12 20:22:57 -0500

    First Commit test

---
+++ TestProject/src/TestProject.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("What is your name");
+//		System.out.println("Hello "+ name);
+//		
+//		JOptionPane.showMessageDialog(null, "Hello "+ name);
+		//this is tesing for commit
+		
+		System.out.printf("","");
+		
+		add test = new add(1,1);
+		System.out.println(test.sum(1, 3));
+	}
+
+}
---
+++ TestProject/src/add.java
@@ -0,0 +1,13 @@
+
+public class add {
+	
+	public add(int i, int j) {
+		int a = i;
+		int b = j;
+	}
+
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+}
